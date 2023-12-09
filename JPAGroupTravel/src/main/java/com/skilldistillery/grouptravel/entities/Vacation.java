00e016f6d88a2f6c3013b1eda21636818b20df67
Zachary Dechant <zdechant0621@gmail.com>
2023-12-08 17:36:49 -0700

    Vacation Entity and J-Unit

---
+++ JPAGroupTravel/src/main/java/com/skilldistillery/grouptravel/entities/Vacation.java
@@ -0,0 +1,127 @@
+package com.skilldistillery.grouptravel.entities;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Vacation {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private String description;
+	@Column(name = "image_url")
+	private String imageUrl;
+	@Column(name = "create_date")
+	private LocalDateTime createDate;
+	@Column(name = "last_update")
+	private LocalDateTime lastUpdate;
+	@Column(name = "start_date")
+	private Date startDate;
+	@Column(name = "end_date")
+	private Date endDate;
+	
+
+	public Vacation() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+	public LocalDateTime getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(LocalDateTime lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vacation other = (Vacation) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Vacation [id=" + id + ", title=" + title + ", description=" + description + ", imageUrl=" + imageUrl
+				+ ", createDate=" + createDate + ", lastUpdate=" + lastUpdate + ", startDate=" + startDate
+				+ ", endDate=" + endDate + "]";
+	}
+
+}
