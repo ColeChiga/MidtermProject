10072618e33a53535e0c0e991a04d860f48b56c4
Zachary Dechant <zdechant0621@gmail.com>
2023-12-08 17:41:48 -0700

    Location Entity and J-Unit

---
+++ JPAGroupTravel/src/main/java/com/skilldistillery/grouptravel/entities/Location.java
@@ -0,0 +1,91 @@
+package com.skilldistillery.grouptravel.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Location {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	@Column(name = "image_url")
+	private String imageUrl;
+	@Column(name = "estimated_cost")
+	private String estimatedCost;
+
+	public Location() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getEstimatedCost() {
+		return estimatedCost;
+	}
+
+	public void setEstimatedCost(String country) {
+		this.estimatedCost = country;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Location [id=" + id + ", name=" + name + ", description=" + description + ", imageUrl=" + imageUrl
+				+ ", estimatedCost=" + estimatedCost + "]";
+	}
+
+}
